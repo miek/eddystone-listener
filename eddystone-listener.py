a88e88c9d854781b8feb84c9932c607dccc33c16
Mike Walters <mike@flomp.net>
2017-06-23 22:52:01 +0100

    Listen for ETLM frames too.

--- eddystone-listener.py
+++ eddystone-listener.py
@@ -1,12 +1,12 @@
 import time
-from beacontools import BeaconScanner, EddystoneEIDFrame
+from beacontools import BeaconScanner, EddystoneEIDFrame, EddystoneEncryptedTLMFrame
 
 def callback(bt_addr, rssi, packet, additional_info):
     print("<%s, %d> %s %s" % (bt_addr, rssi, packet, additional_info))
 
 # scan for all EID frames of beacons
 scanner = BeaconScanner(callback, 
-    packet_filter=EddystoneEIDFrame
+    packet_filter=[EddystoneEIDFrame, EddystoneEncryptedTLMFrame],
 )
 
 scanner.start()
