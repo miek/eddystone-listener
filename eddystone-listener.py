23e090cf13f0dbc1d94d29ea259f12be607bbe8e
Mike Walters <mike@flomp.net>
2017-06-23 22:52:34 +0100

    Store EIDs and link with ETLM frames by bt_addr.

--- eddystone-listener.py
+++ eddystone-listener.py
@@ -1,8 +1,16 @@
 import time
 from beacontools import BeaconScanner, EddystoneEIDFrame, EddystoneEncryptedTLMFrame
+from binascii import hexlify
+
+# TODO: use a more suitable data structure that'll remove old entries
+eids = {}
 
 def callback(bt_addr, rssi, packet, additional_info):
-    print("<%s, %d> %s %s" % (bt_addr, rssi, packet, additional_info))
+    if isinstance(packet, EddystoneEIDFrame):
+        eids[bt_addr] = packet.eid
+    else:
+        print("<%s, %d> %s %s %s" %
+              (bt_addr, rssi, packet, additional_info, hexlify(eids[bt_addr])))
 
 # scan for all EID frames of beacons
 scanner = BeaconScanner(callback, 
